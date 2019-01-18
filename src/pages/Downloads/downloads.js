ef4f37f7a6de73de235927598c25086284f16254
Jameel Kelley <jkelley@iastate.edu>
2019-01-18 11:39:55 -0600

    Updated Downloads Page

--- src/pages/Downloads/downloads.js
+++ src/pages/Downloads/downloads.js
@@ -7,9 +7,20 @@ const Downloads = (props) => {
     <div>
       <h1>Downloads</h1>
 	  <hr/>
+
+    <h1>Pentachoron</h1>
+    <h3>Convert scanned PDF files to text!</h3>
+    <a href="http://jamkelley.com:3001/download/pentachoron">Install (Windows)</a>
+    <h3>Known Bugs</h3>
+    <ul>
+      <li>PDF file must not have spaces in name</li>
+    </ul>
+
+    <hr/>
+    
 	  <h1>PunchClock</h1>
     <h3>Track and take charge of your work hours!</h3>
-    <a href="http://jamkelley.com:3001/download">Install (Windows)</a>
+    <a href="http://jamkelley.com:3001/download/punchclock">Install (Windows)</a>
     </div>
   )
 }
