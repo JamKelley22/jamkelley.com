d3db10db4360f64a8bb62f647839ba5ad30a3324
Jameel Kelley <jkelley@iastate.edu>
2020-06-04 00:22:18 -0500

    Added campaign-zero-widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import { Route, Switch } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+import { CloseCircleTwoTone } from "@ant-design/icons";
 //import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 import { ALL_ROUTES, CustomRoute } from "./routes";
@@ -13,8 +14,26 @@ import "./App.scss";
 library.add(fab);
 
 const App: React.FC = (props: any) => {
+  const [blmOverlayVisible, setBlmOverlayVisible] = useState(true);
   return (
     <div className="App">
+      {blmOverlayVisible && (
+        <div id="blm-overlay">
+          <div id="blm-widget">
+            <CloseCircleTwoTone
+              id="campaign-zero-widget--closebutton"
+              style={{
+                fontSize: "24px",
+                transform: "translate(-5px, 30px)",
+                zIndex: 101,
+              }}
+              twoToneColor="#eb2f96"
+              onClick={() => setBlmOverlayVisible(false)}
+            />
+            <div id="campaign-zero-widget" />
+          </div>
+        </div>
+      )}
       <div id="navagationLinksContainer">
         <Nav />
         {/*props.location.pathname !== HOME.route ? Nav : null*/}
@@ -32,4 +51,4 @@ const App: React.FC = (props: any) => {
   );
 };
 
-export default withRouter(props => <App {...props} />);
+export default withRouter((props) => <App {...props} />);
