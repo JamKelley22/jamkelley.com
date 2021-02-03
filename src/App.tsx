f063b7ebac8eb20d0c4f693109db2d64380242bb
Jameel Kelley <jkelley@iastate.edu>
2021-02-03 00:49:55 -0600

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Route, Switch } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
-import { CloseCircleTwoTone } from "@ant-design/icons";
 //import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 import {
